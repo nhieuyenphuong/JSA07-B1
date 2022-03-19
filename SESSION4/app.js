4d6cfc642f448ecf90dc3cc1a05c06b39425d5f1
nhieuyenphuong <shumi0211@gmail.com>
2022-03-19 10:32:19 +0700

    init repo

---
+++ SESSION4/app.js
@@ -0,0 +1,19 @@
+var name='Phuong '
+let n=prompt('Enter a number: ')
+if (n%2==0){
+    console.log("Even number");
+} else {
+    console.log( 'Odd number ')
+}
+for(i=0;i<10;i++){
+    console.log(i);
+}
+// vong lap chay lui tu 100-10: doc 1 gia tri trong array 
+for (let i = 100; i>9; i--) {
+    console.log(i);
+}
+let students=['phuong','dung']
+for (let i = 0; i < students.length; i++) {
+  if  students == 'phuong '
+    
+}
---
+++ SESSION5/app.js
@@ -0,0 +1,35 @@
+//b1: khai baó dữ liệu của mình
+let data =[ 
+    {
+    id: 123,
+    username:'yenphuong',
+    password:'12345678'
+
+    }
+
+];
+//b2: lấy dữ liệu mà ngta Input
+let usernameInput= document.getElementById('usernname');
+let passwordInput=document.getElementById('password');
+let btnLogin=document.getElementById('btnLogin');
+let error= document.getElementById('errorMessage');
+
+//b3: bắt sự kiện: 
+btnLogin.addEventListener('click', function() {
+    let username = usernameInput.value; 
+    let password = passwordInput.value;
+    //so sánh dữ liệu
+    for( let user of data){
+        if(user.username==username&& user.password==password){
+            window.location.replace('./homepage.html ')
+        } else{
+            error.textContent='Error!';
+            error.style.color='red';
+            error.style.textAlign='center'
+        }
+    }
+
+})
+//let container = document.getElementById('container');
+
+//container.insertAdjacentHTML('afterend', `<h1>${MOVIES[0].plot}</h1>`)
